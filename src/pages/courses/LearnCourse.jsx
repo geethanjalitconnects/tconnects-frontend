525560d200f48d690a8e6dd107579a420e319da0
unknown <geetha272003@gmail.com>
2025-12-04 23:23:24 +0530

    Fix LearnCourse.jsx API import path

--- src/pages/courses/LearnCourse.jsx
+++ src/pages/courses/LearnCourse.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import api from "../../../config/api";
+import api from "../../config/api";
 import "./styles/LearnCourse.css";
 
 import LearnVideoPlayer from "./components/LearnVideoPlayer";
