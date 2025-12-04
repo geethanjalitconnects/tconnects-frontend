359c92ca19902dce4c04d487f2e325fadea9ec51
unknown <geetha272003@gmail.com>
2025-12-04 23:29:58 +0530

    Fix API import paths for all course components

--- src/pages/courses/components/CourseSidebar.jsx
+++ src/pages/courses/components/CourseSidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../../config/api";   // ⭐ ADD THIS
+import api from "../../../config/api";  // ⭐ ADD THIS
 import "../styles/CourseDetails.css";
 import { FaVideo, FaBook, FaClock, FaFileAlt, FaMobileAlt } from "react-icons/fa";
 
