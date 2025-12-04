d71bdccad9ef5872c4d218814eacddc2dee32ecd
unknown <geetha272003@gmail.com>
2025-12-05 01:35:24 +0530

    Fix CourseDetails blank page, update CourseIncludes, curriculum mapping, and routing

--- src/pages/courses/components/CourseIncludes.jsx
+++ src/pages/courses/components/CourseIncludes.jsx
@@ -1,40 +1,20 @@
 import React from "react";
-import "../styles/CourseDetails.css";
-import { FaVideo, FaBook, FaClock, FaFileAlt, FaMobileAlt } from "react-icons/fa";
 
 const CourseIncludes = ({ includes = {} }) => {
   return (
     <div className="course-includes-box">
-      <h2>This course includes</h2>
+      <h2>This course includes:</h2>
 
-      <div className="includes-grid">
+      <ul className="course-includes-list">
+        {includes.hours && <li>⏱ {includes.hours}</li>}
+        {includes.resources && <li>📄 {includes.resources}</li>}
+        {includes.access && <li>📚 {includes.access}</li>}
 
-        <div className="include-item">
-          <FaVideo className="include-icon" />
-          <span>{includes.videos || 0} video lessons</span>
-        </div>
-
-        <div className="include-item">
-          <FaBook className="include-icon" />
-          <span>{includes.modules || 0} modules</span>
-        </div>
-
-        <div className="include-item">
-          <FaClock className="include-icon" />
-          <span>{includes.duration || "N/A"}</span>
-        </div>
-
-        <div className="include-item">
-          <FaFileAlt className="include-icon" />
-          <span>{includes.resources || 0} downloadable resources</span>
-        </div>
-
-        <div className="include-item">
-          <FaMobileAlt className="include-icon" />
-          <span>{includes.access || "Lifetime access"}</span>
-        </div>
-
-      </div>
+        {/* SAFE FALLBACKS */}
+        {!includes.hours && !includes.resources && !includes.access && (
+          <li>No extra materials listed.</li>
+        )}
+      </ul>
     </div>
   );
 };
